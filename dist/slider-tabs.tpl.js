cb83b6a673b3a6357ea4fe917e19c6927353747d
Dominic Watson <dominic@euroslots.com>
2014-09-03 17:49:01 +0200

    initial gh-pages

--- dist/slider-tabs.tpl.js
+++
@@ -1,8 +0,0 @@
-angular.module('angularSliderTabs').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('template/slider-tabs/slider-tabs.tpl.html',
-    "<div class=\"slider-tabs\"><div class=\"viewport\"><div class=\"scroll\" ng-transclude></div></div><div class=\"slide-container\"></div></div>"
-  );
-
-}]);
